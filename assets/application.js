fe6555de9884124fd4a4f2e6cc6ea54672eff232
Habib Ullah <habib.ullah@selisegroup.com>
2025-11-24 23:22:10 +0600

    header footer and home page design and development

---
+++ assets/application.js
@@ -0,0 +1,7 @@
+// Put your application javascript here
+jQuery(document).ready(() => {
+    jQuery('.hero-slider__wrapper').slick({
+      prevArrow: '.hero-slider__button--prev',
+      nextArrow: '.hero-slider__button--next'
+    })
+});
